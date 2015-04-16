620ddb627ca14f70ab03d9b2b25c604ef5d5cc22
Meagen Voss <vossmk@gmail.com>
2015-04-16 16:36:20 -0400

    Implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  # Note this should be set to the actual host name (not localhost).
+  config.action_mailer.default_url_options = { host: 'mvoss-pinteresting.git' }
+
 end
